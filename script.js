20c1166d0b459c3092e2fbe731081b420e9e039e
JonattanCarmona <109824192+JonattanCarmona@users.noreply.github.com>
2022-08-13 23:21:51 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function botonEncriptar(){
+    let texto = document.getElementById("input").value;
+    let textoCifrado = texto.replaceAll("e", "enter").replaceAll("o", "ober").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("u", "ufat");
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("ningunmensaje").style.display = "none";
+    document.getElementById("mensajesintexto").innerHTML = textoCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+function botonDesencriptar(){
+    let texto = document.getElementById("input").value;
+    let textoCifrado = texto.replaceAll("ufat","u").replaceAll("ai", "a").replaceAll("imes", "i").replaceAll("ober", "o").replaceAll("enter", "e");
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("ningunmensaje").style.display = "none";
+    document.getElementById("mensajesintexto").innerHTML = textoCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+function copiar(){
+    var copiado = document.getElementById('mensajesintexto').innerHTML;
+    navigator.clipboard.writeText(copiado)
+        .then(() => {
+        console.log("Text copied to clipboard...")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
+}
