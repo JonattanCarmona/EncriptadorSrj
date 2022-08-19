26f88eb6c46562c004a02ed0e4018ba54fd5aed9
JonattanCarmona <109824192+JonattanCarmona@users.noreply.github.com>
2022-08-18 22:12:16 -0400

    quitar encriptado sin autorizacion

--- script.js
+++ script.js
@@ -3,8 +3,35 @@ function esMayuscula(letra)
     return letra = letra.charCodeAt();
 }  
 function botonEncriptar(){
-
     let texto = document.getElementById("input").value;
+    let veriEncrip = true;
+    labelCancelLoops: for(var index = 0; index < texto.length; index++)
+    {
+        var letraActual = texto.charAt(index);
+        let codigoAscii= esMayuscula(letraActual);
+        if(codigoAscii > 64 && codigoAscii < 90)
+        {
+            alert("La letra " + letraActual + " es mayúscula, solo pueden ser letras minúsculas y sin acentos");
+            veriEncrip = false;
+        }
+        else if (codigoAscii > 122 && codigoAscii < 255)
+        {
+            alert("La letra " + letraActual + " es un carácter especial, solo pueden ser letras minúsculas y sin acentos");
+            veriEncrip = false;
+        }
+    }
+    if(veriEncrip) {
+        var textoCifrado = texto.replaceAll("e", "enter").replaceAll("o", "ober").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("u", "ufat");
+        document.getElementById("muneco").style.display = "none";
+        document.getElementById("ningunmensaje").style.display = "none";
+        document.getElementById("mensajesintexto").innerHTML = textoCifrado;
+        document.getElementById("copiar").style.display = "show";
+        document.getElementById("copiar").style.display = "inherit";
+    }
+}
+function botonDesencriptar(){
+    let texto = document.getElementById("input").value;
+    let veriEncrip = true;
     for(var index = 0; index < texto.length; index++)
     {
         var letraActual = texto.charAt(index);
@@ -12,45 +39,24 @@ function botonEncriptar(){
         if(codigoAscii > 64 && codigoAscii < 90)
         {
             alert("La letra " + letraActual + " es mayúscula, solo pueden ser letras minúsculas y sin acentos");
+            veriEncrip = false;
         }
         if(codigoAscii > 122 && codigoAscii < 255)
-        {
-            alert("La letra " + letraActual + " es un carácter especial, solo pueden ser letras minúsculas y sin acentos");
-        }
-        else{
-            let textoCifrado = texto.replaceAll("e", "enter").replaceAll("o", "ober").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("u", "ufat");
+            {
+                alert("La letra " + letraActual + " es un carácter especial, solo pueden ser letras minúsculas y sin acentos");
+                veriEncrip = false;
+            }
+     }
+     if(veriEncrip) {
+        let textoCifrado = texto.replaceAll("ufat","u").replaceAll("ai", "a").replaceAll("imes", "i").replaceAll("ober", "o").replaceAll("enter", "e");
             document.getElementById("muneco").style.display = "none";
             document.getElementById("ningunmensaje").style.display = "none";
             document.getElementById("mensajesintexto").innerHTML = textoCifrado;
+            document.getElementById("mensajesintexto").style.marginTop = "3em";
             document.getElementById("copiar").style.display = "show";
             document.getElementById("copiar").style.display = "inherit";
-        }
     }
-    
-}
-function botonDesencriptar(){
-    let texto = document.getElementById("input").value;
-    var letraActual = texto.charAt(index);
-    let codigoAscii=esMayuscula(letraActual);
-    if(codigoAscii > 64 && codigoAscii < 90)
-    {
-        alert("La letra " + letraActual + " es mayúscula, solo pueden ser letras minúsculas y sin acentos");
     }
-    if(codigoAscii > 122 && codigoAscii < 255)
-        {
-            alert("La letra " + letraActual + " es un carácter especial, solo pueden ser letras minúsculas y sin acentos");
-        }
-    else{
-        let textoCifrado = texto.replaceAll("ufat","u").replaceAll("ai", "a").replaceAll("imes", "i").replaceAll("ober", "o").replaceAll("enter", "e");
-        document.getElementById("muneco").style.display = "none";
-        document.getElementById("ningunmensaje").style.display = "none";
-        document.getElementById("mensajesintexto").innerHTML = textoCifrado;
-        document.getElementById("mensajesintexto").style.marginTop = "3em";
-        document.getElementById("copiar").style.display = "show";
-        document.getElementById("copiar").style.display = "inherit";
-        }
-    
-}
 function copiar(){
     var copiado = document.getElementById('mensajesintexto').innerHTML;
     navigator.clipboard.writeText(copiado)
